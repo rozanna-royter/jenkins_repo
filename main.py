acc92cb13b99e38d5e8886036236230cb396a7c4
Rozanna Royter <rozzy.royter@gmail.com>
2020-06-14 15:12:54 +0300

    fix

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from time import sleep
 if __name__ == "__main__":
     num = int(sys.argv[1])
     for i in range(num):
-        print (str(i)+ +str(i*i))
+        print(str(i) + ' ' + str(i*i))
         sleep(1)
