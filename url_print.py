1964b25ba6e427b5b334d6a73d6a5f585f8c2a5f
Rozanna Royter <rozzy.royter@gmail.com>
2020-06-14 18:00:19 +0300

    remove $

--- url_print.py
+++ url_print.py
@@ -5,7 +5,7 @@ import os
 def main():
     url_list = os.environ['URLS'].split(',')
     for url in url_list:
-        print(f"====== Printing contents of: ${url} ======")
+        print(f"====== Printing contents of: {url} ======")
         r = requests.get(url)
         print(r.text)
 
