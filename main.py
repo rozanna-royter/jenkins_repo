743766cdf1a747e396a9eb284e81a0a3ea483326
Rozanna Royter <rozzy.royter@gmail.com>
2020-06-14 16:58:27 +0300

    added edited main

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import os
 import sys
 from time import sleep
 
 if __name__ == "__main__":
-    num = int(sys.argv[1])
+    num = int(os.environ['NUM'])
     for i in range(num):
         print(str(i) + ' ' + str(i*i))
         sleep(1)
