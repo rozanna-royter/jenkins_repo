3eab24fbead3919707ef4df6656cb06224e540d8
Rozanna Royter <rozzy.royter@gmail.com>
2020-06-14 17:16:18 +0300

    fixed list creation

--- url_print.py
+++ url_print.py
@@ -3,7 +3,7 @@ import os
 
 
 def main():
-    url_list = os.environ['URLS']
+    url_list = os.environ['URLS'].split(',')
     # url_list = ['http://www.youtube.com','http://www.facebook.com','http://www.baidu.com','http://www.yahoo.com','http://www.amazon.com']
     for url in url_list:
         r = requests.get(url)
